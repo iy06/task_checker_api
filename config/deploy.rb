fc1fbe99e7ff37eb7a75fb7e1cb4fa6a3f792d1c
iy06 <yuki.inoue@di-v.co.jp>
2021-04-05 19:29:49 +0900

    edit Capfile and deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '~> 3.16.0'
 
 set :application, 'task_checker_api'
-set :repo_url, 'git@example.com:iy06/task_checker_api.git'
+set :repo_url, 'git@github.com:iy06/task_checker_api.git'
 
 # Default branch is :master -> set :branch, 'master'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
