2755cde0fd2007883a92752b6bed72a0579a86d2
iy06 <yuki.inoue@di-v.co.jp>
2021-04-05 18:32:51 +0900

    edit production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,6 +5,7 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
+server '35.73.172.212', user: 'ec2-user', roles: %w{app db web}
 # server "db.example.com", user: "deploy", roles: %w{db}
 
 
