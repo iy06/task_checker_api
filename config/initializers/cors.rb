e2e11aa389bf793fc797f53cdf9d53bf27ded01e
iy06 <yuki.inoue@di-v.co.jp>
2021-04-06 15:31:21 +0900

    edit amplify domain cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "https://master.d3g8de0zu4vt37.amplifyapp.com"
 
     resource "*",
       headers: :any,
